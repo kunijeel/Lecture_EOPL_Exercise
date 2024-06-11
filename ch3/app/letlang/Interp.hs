06cab2e7a1ef6e892c8d4ee035489b2874e81e33
Tam Oh <tamhappy18@gmail.com>
2024-06-11 15:04:37 +0900

    Update LET-interpreter

--- ch3/app/letlang/Interp.hs
+++ ch3/app/letlang/Interp.hs
@@ -6,29 +6,33 @@ import Env
 --
 value_of :: Exp -> Env -> ExpVal
 
-value_of (Const_Exp n) env =
-  error "TODO: implement a value_of function"
+value_of (Const_Exp n) env = Num_Val n
 
-value_of (Var_Exp var) env =
-  error "TODO: implement a value_of function"
+value_of (Var_Exp var) env = apply_env env var
 
-value_of (Diff_Exp exp1 exp2) env =
-  error "TODO: implement a value_of function"
+value_of (Diff_Exp exp1 exp2) env = 
+  let num1 = expval_num (value_of exp1 env)
+      num2 = expval_num (value_of exp2 env)
+  in Num_Val (num1 - num2)
   
 value_of (IsZero_Exp exp) env =
-  error "TODO: implement a value_of function"
+  if expval_num (value_of exp env) == 0
+  then Bool_Val True
+  else Bool_Val False
 
 value_of (If_Exp exp1 exp2 exp3) env =
-  error "TODO: implement a value_of function"
+  if expval_bool (value_of exp1 env)
+  then value_of exp2 env
+  else value_of exp3 env
 
 value_of (Let_Exp var exp1 body) env =
-  error "TODO: implement a value_of function"
+  value_of body (extend_env var (value_of exp1 env) env)
     
 
 --
 value_of_program :: Exp -> ExpVal
 
-value_of_program exp = error "TODO: implement a value_of_program function"
+value_of_program exp = value_of exp initEnv
 
 
 --
